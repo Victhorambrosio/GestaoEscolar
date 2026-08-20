22e3202e0adba38ba4c655cd5fdaf01d509018ce
Victhorambrosio <victhor.ambrosio@aluno.senai.br>
2026-08-20 11:30:47 -0300

    Adionado serviço de banco de dados e identidade

---
+++ web.GestaoEscolar/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestaoEscolar;
+
+public class AppDbContext : IdentityDbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options):base(){}
+}
---
+++ web.GestaoEscolar/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace web.GestaoEscolar.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
